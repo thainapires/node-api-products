ea6f3121dafb9901e52259899a681410b686b278
Thainá Pires <thainaspiress@gmail.com>
2020-08-15 20:31:38 -0300

    code

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+//Iniciando o app
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+//Iniciando db
+mongoose.connect('mongodb://localhost:27017/node-api', { useNewUrlParser: true});
+requireDir('./src/models');
+
+//Rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
